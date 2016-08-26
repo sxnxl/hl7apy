27a9a83be5d3354f09562c67bde6ae0badc24c16
Senol Korkmaz <senol.korkmaz@neu.edu.tr>
2016-08-26 10:21:21 +0300

    import setuputils

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-
+import setuptools
 from distutils.core import setup
 from distutils.errors import DistutilsSetupError
 
